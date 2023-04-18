9deb0d1f04223655a15dd451cf4cf4e436cfd12e
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-04-18 09:17:26 +0700

    Implementing `ManyToOne` relationship

--- src/test/java/site/masadamsahid/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/site/masadamsahid/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import site.masadamsahid.spring.data.jpa.tutorial.entity.Course;
+import site.masadamsahid.spring.data.jpa.tutorial.entity.Teacher;
 
 import java.util.List;
 
@@ -17,6 +18,22 @@ class CourseRepositoryTest {
   public void printCourses(){
     List<Course> courses = courseRepository.findAll();
     System.out.println("courses = " + courses);
+  }
+  
+  @Test
+  public void saveCourseWithTeacher(){
+    Teacher teacher = Teacher.builder()
+      .firstName("Retsam")
+      .lastName("Nohtyp")
+      .build();
+    
+    Course course = Course.builder()
+      .title("Python")
+      .credit(6)
+      .teacher(teacher)
+      .build();
+    
+    courseRepository.save(course);
   }
   
 }
