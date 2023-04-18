806016dfed67c47b74b071c589e13d1628d45d26
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-04-18 09:56:12 +0700

    using pagination and sorting

--- src/test/java/site/masadamsahid/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/site/masadamsahid/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -3,6 +3,9 @@ package site.masadamsahid.spring.data.jpa.tutorial.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import site.masadamsahid.spring.data.jpa.tutorial.entity.Course;
 import site.masadamsahid.spring.data.jpa.tutorial.entity.Teacher;
 
@@ -34,6 +37,33 @@ class CourseRepositoryTest {
       .build();
     
     courseRepository.save(course);
+  }
+  
+  @Test
+  public void findAllPagination(){
+    Pageable firstPageWithThreeRecords = PageRequest.of(0,3);
+    Pageable secondPageWithTwoRecords = PageRequest.of(1,2);
+    
+    List<Course> courses = courseRepository.findAll(secondPageWithTwoRecords).getContent();
+    
+    long totalElements = courseRepository.findAll(secondPageWithTwoRecords).getTotalElements();
+    long totalPages = courseRepository.findAll(secondPageWithTwoRecords).getTotalPages();
+    
+    System.out.println("courses = " + courses);
+    System.out.println("totalElements = " + totalElements);
+    System.out.println("totalPages = " + totalPages);
+  }
+  
+  @Test
+  public void findAllSorting(){
+    Pageable sortByTitle = PageRequest.of(0, 2, Sort.by("title"));
+    Pageable sortByCreditDesc = PageRequest.of(0,2, Sort.by("credit").descending());
+    Pageable sortByTitleAndCreditDesc =
+      PageRequest.of(0,2, Sort.by("title").descending().and(Sort.by("credit")));
+    
+    List<Course> courses = courseRepository.findAll(sortByTitle).getContent();
+    
+    System.out.println("courses = " + courses);
   }
   
 }
