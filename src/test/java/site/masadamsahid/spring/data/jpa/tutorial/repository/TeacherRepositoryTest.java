c27ca379411a8f95b87e2c8c3732cdcc57e01bde
Maulana Adam Sahid <masadamsahid@gmail.com>
2023-04-18 08:42:12 +0700

    add `Teacher` and make it's relation to `Course` as `one-to-many` relationship

---
+++ src/test/java/site/masadamsahid/spring/data/jpa/tutorial/repository/TeacherRepositoryTest.java
@@ -0,0 +1,41 @@
+package site.masadamsahid.spring.data.jpa.tutorial.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import site.masadamsahid.spring.data.jpa.tutorial.entity.Course;
+import site.masadamsahid.spring.data.jpa.tutorial.entity.Teacher;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+  
+  @Autowired
+  private TeacherRepository teacherRepository;
+  
+  @Test
+  public void saveTeacher(){
+  
+    Course courseDBA = Course.builder()
+      .title("DBA")
+      .credit(5)
+      .build();
+    
+    Course courseJava = Course.builder()
+      .title("Java")
+      .credit(6)
+      .build();
+    
+    Teacher teacher = Teacher.builder()
+      .firstName("Orezdiov")
+      .lastName("Oruen")
+      .courses(List.of(courseDBA, courseJava))
+      .build();
+    
+    teacherRepository.save(teacher);
+  }
+  
+}
